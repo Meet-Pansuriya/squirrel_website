9506200db1894534132a6b7f7d3f7c388d889a08
Meet-Pansuriya <109430037+Meet-Pansuriya@users.noreply.github.com>
2025-11-01 21:58:32 +0530

    gh pages

--- src/types/react-helmet.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "react-helmet" {
-  import type { ReactNode } from "react";
-
-  export class Helmet extends React.Component<{ children?: ReactNode }> {}
-  export interface HelmetProps {
-    children?: ReactNode;
-  }
-}
